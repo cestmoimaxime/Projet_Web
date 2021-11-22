83dd1b6543d6cded85cf9172cf0feff9475c1879
cestmoimaxime <94811875+cestmoimaxime@users.noreply.github.com>
2021-11-22 18:14:32 +0100

    premiere modif

--- ProjetWeb/connect.php
+++
@@ -1,9 +0,0 @@
-    <?php
-        $link = mysqli_connect('localhost', 'username', 'motdepasse', 'icone');
-
-        if (!$link) {
-            die('Erreur de connexion');
-      } else {
-            echo 'Succès... ';
-      }
-    ?>
--- README.md
+++
@@ -1,1 +0,0 @@
-# laughing-rotary-phone
